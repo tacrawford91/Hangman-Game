2df996ba6a1b508e21877404548ea7c1c5f45717
Troy Crawford <Troy@lechler.de>
2017-11-28 21:42:04 -0600

    Psuedo Code of Game 1

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,23 @@
 console.log("Hello World!");
+
+// First Create Word Bank
+            //EACH WORD SHOULD BE AN ARRAY WITH EACH LETTER BEING AN Element - example var word1 = [ p,i,z,z,a]; 
+            // then var wordBank = [word1, word2, etc]
+
+//generate random number between 1 and total numbers of words and set number to var i -- var i = Floor (total#ofwords*(math.randomnumber)) 
+
+//Set Selected Word -- selectedWord = wordBank[i]
+            //Display Selcted Word  - selectedWord
+
+// listen for keystroke from user (userKeystroke)
+
+//Set number allowed of number of guesses
+
+//Check user guess keystrokes against selectedWord[i], 
+        //display Letter in correct Way
+        //subtract from one number of guesses left
+        // play success sound 
+// else display key stoke as Letters Guessed
+        //subtract from one number of guesses left
+        // play defeat sound
+        //update hangman image 

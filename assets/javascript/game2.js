b076b7c3fbffd94b7fdd377a2d8494050a90f449
Troy Crawford <Troy@lechler.de>
2017-11-29 21:36:40 -0600

    Displaying Correct Letters and letters guessed, Need to work in count/for loop

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -1,53 +1,45 @@
-// Create word bank - 
-    //var word = "string1"
-var dogSays = "woof";
-var catSays = "meow";
-    //var wordBank = [string1, string 2, etc.]
-var wordBank = [dogSays,catSays];
-// Generate selectedWord - 
-    // Generate random number via var randomIndex = Math.Floor(wordBank.length*randomnumber generation);
-    // var selecetedWord = wordBank[randomIndex];
-var selectedWord = wordBank[Math.floor((Math.random()*wordBank.length))];
-console.log(selectedWord);
-//  Display selectedWord as dashes in html
-var dashedArray = [];
-for (var i = 0; i< selectedWord.length; i++){
-    dashedArray[i]="_ ";
-}
+
 console.log(dashedArray);
 document.querySelector(".selected").innerHTML = `
 <h1> ${dashedArray.join(" ")} </h1>
 `;
-
+count = 10;
 // Listen for user key event
 document.onkeyup = function(event) {
-    var usersGuess = event.key;
-    //save to varible userKey = keyevent
+        var usersGuess = event.key;
+        //save to varible userKey = keyevent
     console.log(usersGuess);
-    //check if key is in selectedWord - selectedWord.includes(userKey)
-    //if True
-    if (selectedWord.includes(usersGuess) === true) {
-        console.log("MATCHHHHHH!");
-    //subtract 1 guess Update display
-    count--
-    console.log("count is" + count);
-    document.querySelector(".count").innerHTML = `
-    <h1> Turns Left: ${count} </h1>
-    `;
-    //play victory sound
-    //check for word completion
-    } else {   // if false
-            // subtract 1 from guess count
-    // add letter to guessed list
-    //update hangman image
-    //play loser sound
-    console.log("key is false");
-    count--
-    console.log("count is" + count);
-    document.querySelector(".count").innerHTML = `
-    <h1> Turns Left: ${count} </h1>
-    `;
-    }
+        //Setting up count - 
+        //check if key is in selectedWord - selectedWord.includes(userKey)
+        //if True
+        for (var i = 0; i < count; i++ ) {
+            if (selectedWord.includes(usersGuess) === true) {
+                console.log("MATCHHHHHH!");
+                //update dashedArray
+                // find index of matched letter
+                var matchedIndex = selectedWord.indexOf(usersGuess);
+                console.log("the matched index is " + matchedIndex)
+                dashedArray[matchedIndex] =usersGuess;
+                console.log(dashedArray);
+                document.querySelector(".selected").innerHTML = `
+                <h1> ${dashedArray.join(" ")} </h1>
+                `;
+                console.log(count);
+            //subtract 1 guess Update display
+            //play victory sound
+            //check for word completion
+            } else {   // if false
+                // add letter to guessed list
+                guessedLetters.push(usersGuess);
+                document.querySelector(".guessed").innerHTML = `
+                <h1> ${guessedLetters} </h1>
+                `;
+                // subtract 1 from guess count
+                //update hangman image
+                //play loser sound
+            console.log("key is false");
+            };
+        };
 };
 //check if key is in selectedWord - selectedWord.includes(userKey)
 

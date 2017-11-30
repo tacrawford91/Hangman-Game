ea681e9cc02a615091519f124785cccdaf309797
Troy Crawford <Troy@lechler.de>
2017-11-30 08:16:33 -0600

    Added count and win/loss logic. Need to refactor. Keep in mind double letter words, and not changing count if letter already successfully guessed

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -1,4 +1,20 @@
-
+// Create word bank - 
+    //var word = "string1"
+var dogSays = "arf";
+var catSays = "meow";
+    //var wordBank = [string1, string 2, etc.]
+var wordBank = [dogSays,catSays];
+// Generate selectedWord - 
+    // Generate random number via var randomIndex = Math.Floor(wordBank.length*randomnumber generation);
+    // var selecetedWord = wordBank[randomIndex];
+var selectedWord = wordBank[Math.floor((Math.random()*wordBank.length))];
+console.log(selectedWord);
+//  Display selectedWord as dashes in html
+var guessedLetters = [];
+var dashedArray = [];
+for (var i = 0; i< selectedWord.length; i++){
+    dashedArray[i]="_ ";
+}
 console.log(dashedArray);
 document.querySelector(".selected").innerHTML = `
 <h1> ${dashedArray.join(" ")} </h1>
@@ -12,8 +28,8 @@ document.onkeyup = function(event) {
         //Setting up count - 
         //check if key is in selectedWord - selectedWord.includes(userKey)
         //if True
-        for (var i = 0; i < count; i++ ) {
-            if (selectedWord.includes(usersGuess) === true) {
+        for (var i = 0; i < 1; i++ ) {
+            if (selectedWord.includes(usersGuess) === true && count > 1 && dashedArray.includes("_ ") === true && guessedLetters.includes(usersGuess)=== false) {
                 console.log("MATCHHHHHH!");
                 //update dashedArray
                 // find index of matched letter
@@ -24,21 +40,38 @@ document.onkeyup = function(event) {
                 document.querySelector(".selected").innerHTML = `
                 <h1> ${dashedArray.join(" ")} </h1>
                 `;
+                //subtract 1 guess Update display
+                count--
                 console.log(count);
-            //subtract 1 guess Update display
-            //play victory sound
-            //check for word completion
-            } else {   // if false
+                document.querySelector(".count").innerHTML = `
+                <h1> Turns Left: ${count} </h1>
+                `;
+                //play victory sound
+                //check for word completion
+                if (dashedArray.includes("_ ") === false && count > 0) {
+                    console.log("winner winner chicken dinner");
+                }
+            } else if (selectedWord.includes(usersGuess) === false && count > 1 && dashedArray.includes("_ ") === true && guessedLetters.includes(usersGuess)=== false) {   // if false
                 // add letter to guessed list
                 guessedLetters.push(usersGuess);
                 document.querySelector(".guessed").innerHTML = `
                 <h1> ${guessedLetters} </h1>
                 `;
                 // subtract 1 from guess count
+                count--
+                console.log(count);
+                document.querySelector(".count").innerHTML = `
+                <h1> Turns Left: ${count} </h1>
+                `;
                 //update hangman image
                 //play loser sound
-            console.log("key is false");
-            };
+                console.log("key is false");
+            } else if (guessedLetters.includes(usersGuess)=== true) {
+                console.log("Already Guessed!!!!")
+            }
+            } else { 
+                console.log("Game Over");
+            }
         };
 };
 //check if key is in selectedWord - selectedWord.includes(userKey)
